2d0db5cc4fcc0631aebb95f1ab4296e8a4b74fd5
milutin <MilutinMM@gmail.com>
2020-03-06 17:42:02 +0100

    run_udf added

--- WUR_BFAST_UseCase/R/test_BFAST.R
+++ WUR_BFAST_UseCase/R/test_BFAST.R
@@ -5,11 +5,12 @@ library(openeo)
 
 # enter valid credentials
 euracHost = "https://openeo.eurac.edu"
-user = "user"
-password = "user"
+user = "guest"
+password = "guest123"
 api_versions(url=euracHost)
-eurac = connect(host = euracHost, version="0.4.2", user = user,password = password, login_type = "basic")
 
+# eurac = connect(host = euracHost, version="0.4.2", user = user, password = password, login_type = "basic")
+eurac = connect(host = euracHost)
 
 
 # Build a process graph:
@@ -24,8 +25,12 @@ s2 = p$load_collection(id = p$data$openEO_S2_32632_10m_L2A,
                           temporal_extent = c("2018-06-04T00:00:00Z","2018-06-23T00:00:00Z"))
 
 
+list_udf_runtimes(eurac)
+describe_process(con = eurac,"run_udf")
+
 udfName = "BFAST_udf.R"
 udfCode = readChar(udfName, file.info(udfName)$size)
 
-send_udf(s2, udfCode, host = "", port = NULL,
-         language = "R", debug = FALSE, download_info = FALSE)
+# send_udf(s2, udfCode, host = "", port = NULL, language = "R", debug = FALSE, download_info = FALSE)
+
+p$run_udf(data = s2, udf = udfCode, runtime = "R")
