c2970ae610487c1249078845b3a236baf1a3b98d
milutin <MilutinMM@gmail.com>
2020-04-27 16:30:22 +0200

    cenged applied to move from pseudo code to the rel case (currently only offline testing)

--- WUR_BFAST_UseCase/Python/BFAST_udf.py
+++ WUR_BFAST_UseCase/Python/BFAST_udf.py
@@ -1,32 +1,42 @@
 def bfast4openeo(udf_data):
-    udf_data_xr = udf_data.get_array()
+    #
     from bfast import BFASTMonitor
     from bfast.utils import crop_data_dates
     from datetime import datetime
     import xarray as xr
+    import pandas as pd
+    #
     start_hist = datetime(2017, 1, 1)
     start_monitor = datetime(2019, 1, 1)
-    end_monitor = datetime(2020, 1, 1)
-    # --- revisit parsing of time/labels -----
-    dates = udf_data_xr.coords['time']
-    # ---------------------------------------
-    data, dates = crop_data_dates(udf_data_xr.values, dates, start_hist, end_monitor)
-    # --------------------------
+    end_monitor = datetime(2019, 12, 29)
+    # get the dates from the data cube:
+    dates = [pd.Timestamp(date).to_pydatetime() for date in  udf_data.coords['time'].values]
+    # pre-processing - crop the input data cube according to the history and monitor periods:
+    data, dates = crop_data_dates(udf_data.values, dates, start_hist, end_monitor)
+    # !!! Note !!! that data has the shape 91, and not 92 for our dataset. The reason is the definition in
+    # the bfast utils.py script where the start_hist is set < than dates, and not <= than dates.
+    # -------------------------------------
+    # specify the BFASTmonitor parameters:
     model = BFASTMonitor(
         start_monitor,
         freq=365,
         k=3,
+        verbose=1,
         hfrac=0.25,
         trend=False,
         level=0.05,
-        backend='Python',
+        backend='python',
         device_id=0,
     )
-    model.fit(data, dates, n_chunks=1, nan_value=-32768)
-    breaks_xr = xr.DataArray(model.breaks, coords=[udf_data_xr.coords['x'],udf_data_xr.coords['y']], dim=['x', 'y'])
-    udf_data.set_array(breaks_xr)
-
-bfast4openeo(data)
+    # run the monitoring:
+    # !!! question !!! can we get more informative progress bar for the below run?
+    model.fit(data, dates, n_chunks=1, nan_value=-9999)
+    # get the detected breaks as an xarray Data Array:
+    # !!! question !!! are those breaks identical to the breaks we get from R script?
+    breaks_xr = xr.DataArray(model.breaks,
+                             coords=[udf_data.coords['y'].values,udf_data.coords['x'].values],
+                             dims=['y', 'x'])
+    return breaks_xr
 
 
 
--- WUR_BFAST_UseCase/Python/test_BFAST_offline.py
+++ WUR_BFAST_UseCase/Python/test_BFAST_offline.py
@@ -1,7 +1,10 @@
 import os
+import sys
 import xarray as xr
 import pandas as pd
-
+#
+sys.path.append('/home/milutin/rStudioProjects/final-user-workshop/WUR_BFAST_UseCase/Python/')
+from BFAST_udf import bfast4openeo
 
 # set the working dir:
 os.chdir('/home/milutin/rStudioProjects/final-user-workshop/WUR_BFAST_UseCase/Python/')
@@ -10,10 +13,17 @@ os.chdir('/home/milutin/rStudioProjects/final-user-workshop/WUR_BFAST_UseCase/Py
 da = xr.open_rasterio('s1_vh_2017_2019_aoi.tif')\
     .rename({'band': 'time'})
 
-# assign the time Astamps:
+# assign the time stamps:
 da.coords['time'] = pd.date_range(start='1/1/2017', end='29/12/2019', freq='12D')
 
+# test the  bfast4openeo function:
+breaks = bfast4openeo(da)
 
+
+# run the "BFAST_udf.py" script "offline":
+# import sys
+# import subprocess
+# subprocess.call([sys.executable, 'BFAST_udf.py', da])
 
 
 
