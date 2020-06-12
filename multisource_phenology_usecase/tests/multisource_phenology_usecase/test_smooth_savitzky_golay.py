3e57d6f88cfea29651bfda524ba9cf3be8c0d024
Tamas Banyai <tamas.banyai@vito.be>
2020-06-12 18:35:56 +0200

    EP-3326 conforming with apply_dimension contract

--- multisource_phenology_usecase/tests/multisource_phenology_usecase/test_smooth_savitzky_golay.py
+++ multisource_phenology_usecase/tests/multisource_phenology_usecase/test_smooth_savitzky_golay.py
@@ -14,7 +14,8 @@ class TestSmoothing(unittest.TestCase):
 
     def testUDF_SavitzkyGolaySmooth(self):
         gridsize=8
-        datacube=load_UDF_Data(os.path.join(os.path.dirname(__file__),'input_'+str(gridsize)))
+#        datacube=load_UDF_Data(os.path.join(os.path.dirname(__file__),'input_'+str(gridsize)))
+        datacube=load_UDF_Data(os.path.join(os.path.dirname(__file__),'nan_phenology/data_f44891612e7d415b970f90a6f28b050f'))
         refcube=load_UDF_Data(os.path.join(os.path.dirname(__file__),'smoothed_'+str(gridsize)))
         smoothedcube=udf_smooth_savitzky_golay.apply_hypercube(datacube, {})
         xarray.testing.assert_allclose(smoothedcube.get_array(), refcube.get_array())
--- multisource_phenology_usecase/udf_evi.py
+++ multisource_phenology_usecase/udf_evi.py
@@ -4,77 +4,42 @@ from openeo_udf.api.datacube import DataCube
 from typing import Dict
 
 
+# REDUCE_BANDS_VERSION
+# def apply_hypercube(cube: DataCube, context: Dict) -> DataCube:
+# 
+#     # get the xarray    
+#     array=cube.get_array()
+# 
+#     # compute reflectance
+#     B02=array[:,array.bands.values.tolist().index('TOC-B02_10M')]
+#     B04=array[:,array.bands.values.tolist().index('TOC-B04_10M')]
+#     B08=array[:,array.bands.values.tolist().index('TOC-B08_10M')]
+#     result = (2.5 * (B08 - B04)) / ((B08 + 6.0 * B04 - 7.5 * B02) + 10000.0*1.0)
+# 
+#     return DataCube(result)
+
+
 def apply_hypercube(cube: DataCube, context: Dict) -> DataCube:
 
     import numpy
-    import xarray
 
     # get the xarray    
     array=cube.get_array()
-    result=xarray.zeros_like(array, numpy.float64)
-
-#     # create a copy
-#     result=array.copy()    
-#     shape=list(array.shape)
-#     shape[array.dims.index('bands')]=1
-#     result=xarray.DataArray(
-#         numpy.full(shape, 0., numpy.float64),
-#         dims=array.dims,
-#         coords={
-#             't': array.coords['t'].values,
-#             'bands': ['evi']
-#         }
-#     )
 
     # compute reflectance
     B02=array[:,array.bands.values.tolist().index('TOC-B02_10M')]
     B04=array[:,array.bands.values.tolist().index('TOC-B04_10M')]
     B08=array[:,array.bands.values.tolist().index('TOC-B08_10M')]
     result = (2.5 * (B08 - B04)) / ((B08 + 6.0 * B04 - 7.5 * B02) + 10000.0*1.0)
-    
-    #result[:,1:]= result[:,0]
 
     # give proper name for results
     #season=result.expand_dims('bands',3).assign_coords(bands=['evi']).astype(numpy.float64)
-    #result=result.expand_dims('bands',1).assign_coords(bands=['evi']).astype(numpy.float64)
+#    result=result.expand_dims('bands',1).assign_coords(bands=['evi']).astype(numpy.float64)
+#    result=result.assign_coords(bands=['evi']).astype(numpy.float64)
     
 #    result=result.assign_coords('bands'=array.bands)
     
-    result=xarray.DataArray(result,coords={'t': array.coords['t'].values})
+    #result=xarray.DataArray(result,coords={'t': array.coords['t'].values})
     
     return DataCube(result)
-
-
-# def apply_hypercube(cube: DataCube, context: Dict) -> DataCube:
-# 
-# #    import numpy
-# 
-#     # get the xarray    
-#     array=cube.get_array()
-# 
-#     # create a copy
-#     result=array.copy()    
-# #     shape=list(array.shape)
-# #     shape[array.dims.index('bands')]=1
-# #     result=xarray.DataArray(
-# #         numpy.full(shape, 0., numpy.float64),
-# #         dims=array.dims,
-# #         coords={
-# #             't': array.coords['t'].values,
-# #             'bands': ['evi']
-# #         }
-# #     )
-# 
-#     # compute reflectance
-#     B02=array[:,array.bands.values.tolist().index('TOC-B02_10M')]
-#     B04=array[:,array.bands.values.tolist().index('TOC-B04_10M')]
-#     B08=array[:,array.bands.values.tolist().index('TOC-B08_10M')]
-#     result.values[:,0] = (2.5 * (B08 - B04)) / ((B08 + 6.0 * B04 - 7.5 * B02) + 10000.0*1.0)
-#     result.values[:,1:] = 0.
-# 
-#     # give proper name for results
-#     #season=result.expand_dims('bands',3).assign_coords(bands=['evi']).astype(numpy.float64)
-#     #result=result.assign_coords(bands=['evi','empty1','empty2']).astype(numpy.float64)
-#     
-#     return DataCube(result)
 
