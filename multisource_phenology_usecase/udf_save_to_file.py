d8361679d5ec5c96f167db9d7113a1979926bd68
Tamas Banyai <tamas.banyai@vito.be>
2020-06-22 12:36:54 +0200

    EP-3337 debug feature improvements

--- multisource_phenology_usecase/udf_save_to_file.py
+++ multisource_phenology_usecase/udf_save_to_file.py
@@ -9,34 +9,39 @@ def apply_hypercube(cube: DataCube, context: Dict) -> DataCube:
     import uuid
 
     # naming properties
-    if context is not None:
-        uid=uuid.uuid4().hex
-        user=context['user']
-        label=context['label']
-        prefix=context['prefix']
+    uid=uuid.uuid4().hex
+    user=context['user']
+    label=context['label']
+    prefix=context['prefix']
 
-        with open(prefix+user+'/'+label+'_'+uid+'.pre','w') as fw:
-            fw.write("BLABLA")
-            fw.flush()
-        
-        # extract xarray
-        inarr=cube.get_array()
-    
-        # write schema
-        indict=inarr.to_dict(data=False)
-        with open(prefix+user+'/'+label+'_'+uid+'.schema','w') as fw: 
-            json.dump(indict, fw)
-            fw.flush()
-        
-        # write data
-        indict=cube.get_array().to_dict()
-        with open(prefix+user+'/'+label+'_'+uid+'.json','w') as fw: 
-            json.dump(indict, fw, default=str)
-            fw.flush()
-    
-        with open(prefix+user+'/'+label+'_'+uid+'.post','w') as fw:
-            fw.write("BLABLA")
-            fw.flush()
+    with open(prefix+user+'/'+label+'_'+uid+'.pre','w') as fw:
+        fw.write("BLABLA")
+        fw.flush()
+     
+    # write context
+    with open(prefix+user+'/'+label+'_'+uid+'.ctxt','w') as fw:
+        if (context is not None):
+            fw.write(json.dumps(context,indent=4))
+        fw.flush()
+
+    # extract xarray
+    inarr=cube.get_array()
+  
+    # write schema
+    indict=inarr.to_dict(data=False)
+    with open(prefix+user+'/'+label+'_'+uid+'.schema','w') as fw: 
+        json.dump(indict, fw)
+        fw.flush()
+      
+    # write data
+    indict=cube.get_array().to_dict()
+    with open(prefix+user+'/'+label+'_'+uid+'.json','w') as fw: 
+        json.dump(indict, fw, default=str)
+        fw.flush()
+ 
+    with open(prefix+user+'/'+label+'_'+uid+'.post','w') as fw:
+        fw.write("BLABLA")
+        fw.flush()
 
     # behave transparently
     return cube
